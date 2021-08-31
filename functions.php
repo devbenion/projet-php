99eb1535a11b9c8e9a280c896cd0578442fd02be
Traore <traoremoussa377@gmail.com>
2021-08-31 10:01:42 -0400

    test

--- functions.php
+++ functions.php
@@ -2,4 +2,4 @@
 
 $Organismes = __DIR__ . DIRECTORY_SEPARATOR . 'cli' . DIRECTORY_SEPARATOR . 'Organismes.txt';
 $resource = fopen($Organismes, 'r');
-echo fread($resource, 2));
+echo fread($resource, 2);
